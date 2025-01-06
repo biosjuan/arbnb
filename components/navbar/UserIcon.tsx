73bad4c8a6177f9cc03e01dcf61b532213ccbefe
biosjuan <hsoftwaredeveloperlee@gmail.com>
2025-01-06 09:49:38 -0800

    first commit

---
+++ components/navbar/UserIcon.tsx
@@ -0,0 +1,9 @@
+import { LuUser } from 'react-icons/lu';
+
+function UserIcon() {
+  return (
+    <LuUser className='w-6 h-6 bg-primary rounded-full object-cover text-white' />
+  );
+}
+
+export default UserIcon;
