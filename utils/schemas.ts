1c8d33661050e2a32ee9d3447e030d1383c8fdf0
biosjuan <hsoftwaredeveloperlee@gmail.com>
2025-01-13 11:34:13 -0800

    added image upload

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -12,3 +12,36 @@ export const profileSchema = z.object({
     message: 'username must be at least 2 characters',
   }),
 });
+
+export function validateWithZodSchema<T>(
+  schema: ZodSchema<T>,
+  data: unknown
+): T {
+  console.log('result', data);
+  const result = schema.safeParse(data);
+  console.log('result', result);
+  if (!result.success) {
+    const errors = result.error.errors.map((error) => error.message);
+    throw new Error(errors.join(','));
+  }
+  return result.data;
+}
+
+export const imageSchema = z.object({
+  image: validateFile(),
+});
+
+function validateFile() {
+  const maxUploadSize = 1024 * 1024;
+  const acceptedFilesTypes = ['image'];
+  return z
+    .instanceof(File)
+    .refine((file) => {
+      return !file || file.size <= maxUploadSize;
+    }, 'File size must be less than 1 MB')
+    .refine((file) => {
+      return (
+        !file || acceptedFilesTypes.some((type) => file.type.startsWith(type))
+      );
+    }, 'File must be an image');
+}
