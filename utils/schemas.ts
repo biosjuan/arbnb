d2dd7a770c3d1039bffe14414a20812eadfd420b
biosjuan <hsoftwaredeveloperlee@gmail.com>
2025-01-15 11:10:05 -0800

    finsh with create property

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -19,7 +19,7 @@ export function validateWithZodSchema<T>(
 ): T {
   console.log('result', data);
   const result = schema.safeParse(data);
-  console.log('result', result);
+  // console.log('result', result);
   if (!result.success) {
     const errors = result.error.errors.map((error) => error.message);
     throw new Error(errors.join(','));
@@ -45,3 +45,49 @@ function validateFile() {
       );
     }, 'File must be an image');
 }
+
+export const propertySchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: 'name must be at least 2 characters.',
+    })
+    .max(100, {
+      message: 'name must be less than 100 characters.',
+    }),
+  tagline: z
+    .string()
+    .min(2, {
+      message: 'tagline must be at least 2 characters.',
+    })
+    .max(100, {
+      message: 'tagline must be less than 100 characters.',
+    }),
+  price: z.coerce.number().int().min(0, {
+    message: 'price must be a positive number.',
+  }),
+  category: z.string().min(2, { message: 'category' }),
+  description: z.string().refine(
+    (description) => {
+      const wordCount = description.split(' ').length;
+      return wordCount >= 10 && wordCount <= 1000;
+    },
+    {
+      message: 'description must be between 10 and 1000 words.',
+    }
+  ),
+  country: z.string(),
+  guests: z.coerce.number().int().min(0, {
+    message: 'guest amount must be a positive number.',
+  }),
+  bedrooms: z.coerce.number().int().min(0, {
+    message: 'bedrooms amount must be a positive number.',
+  }),
+  beds: z.coerce.number().int().min(0, {
+    message: 'beds amount must be a positive number.',
+  }),
+  baths: z.coerce.number().int().min(0, {
+    message: 'baths amount must be a positive number.',
+  }),
+  amenities: z.string(),
+});
